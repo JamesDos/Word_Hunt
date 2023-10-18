4297ef7363b6585dff37eb2865a1b1313e237971
Nelson <nz235@cornell.edu>
2023-10-18 17:40:19 -0400

    Updated random_char - Created a cdf that matches the occurrence of that letter in the English language

--- bin/main.ml
+++ bin/main.ml
@@ -16,9 +16,46 @@ let () =
   main ();
   Bogue.quit () *)
 
+let () = Random.self_init ()
+
 let random_char () =
-  let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" in
-  chars.[Random.int (String.length chars)]
+  let cdf =
+    [
+      (0.111607, 'E');
+      (0.196573, 'A');
+      (0.272382, 'R');
+      (0.347830, 'I');
+      (0.419465, 'O');
+      (0.488974, 'T');
+      (0.555518, 'N');
+      (0.612869, 'S');
+      (0.667762, 'L');
+      (0.713150, 'C');
+      (0.749458, 'U');
+      (0.783302, 'D');
+      (0.814973, 'P');
+      (0.845102, 'M');
+      (0.875136, 'H');
+      (0.899841, 'G');
+      (0.920561, 'B');
+      (0.938682, 'F');
+      (0.956461, 'Y');
+      (0.969360, 'W');
+      (0.980376, 'K');
+      (0.990450, 'V');
+      (0.993352, 'X');
+      (0.996074, 'Z');
+      (0.998039, 'J');
+      (1.000000, 'Q');
+    ]
+  in
+  let rand = Random.float 1.0 in
+  let rec find_char letter =
+    match letter with
+    | [] -> failwith "Should not happen"
+    | (p, c) :: t -> if rand <= p then c else find_char t
+  in
+  find_char cdf
 
 let board = Array.make_matrix 4 4 'a'
 
