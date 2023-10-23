4890f4e8ae785a3ef4c0caaf3458c7cb3738505e
Nelson <nz235@cornell.edu>
2023-10-23 15:39:58 -0400

    Removed code - Removed useless code from game.ml

--- bin/game.ml
+++ bin/game.ml
@@ -5,24 +5,6 @@ module GameBoard = Builder.BuildBoard
 
 let board = GameBoard.game_board
 
-let pp_tuple s =
-  match s with x, y -> "\"" ^ string_of_int x ^ " " ^ string_of_int y ^ "\""
-
-(** [pp_list pp_elt lst] pretty-prints list [lst], using [pp_elt] to
-    pretty-print each element of [lst]. *)
-let pp_list pp_elt lst =
-  let pp_elts lst =
-    let rec loop n acc = function
-      | [] -> acc
-      | [ h ] -> acc ^ pp_elt h
-      | h1 :: (h2 :: t as t') ->
-          if n = 100 then acc ^ "..." (* stop printing long list *)
-          else loop (n + 1) (acc ^ pp_elt h1 ^ "; ") t'
-    in
-    loop 0 "" lst
-  in
-  "[" ^ pp_elts lst ^ "]"
-
 let eval_word word =
   let string_to_tuple_list str =
     let tuple_pattern = "\\(([^,]+),([^)]+)\\)" in
