22f7d523004fa144f2122621ee6c4dc8d1638c17
James Tu <jt737@cornell.edu>
2023-10-23 21:15:46 -0400

    Completed REPL in game.ml
    - Completed game.repl so that user an input into program multiple times
    - Added yaml file

--- bin/game.ml
+++ bin/game.ml
@@ -5,6 +5,25 @@ module GameBoard = Builder.BuildBoard
 
 let board = GameBoard.game_board
 
+let eval_input str =
+  let string_to_tuple_list str =
+    str |> String.split_on_char ';'
+    |> List.map (fun s -> Scanf.sscanf s "(%d,%d)" (fun x y -> (x, y)))
+  in
+  BuildBoard.is_valid_word2 (string_to_tuple_list str) board
+
+(* read-eval-print loop *)
+let rec repl (eval : string -> bool) : unit =
+  print_string "> ";
+  let input = read_line () in
+  match input with
+  | "" -> print_endline "Ending Game"
+  | _ ->
+      (let res = input |> eval in
+       if res then "Valid Word" else "Invalid Word")
+      |> print_endline;
+      repl eval
+
 (*********** command line interface ***********)
 let () =
   print_endline "\n\nWelcome to Word Hunt!\n";
@@ -17,19 +36,8 @@ let () =
      word. \n\
      For example,(0,0);(0,1);(0,2) would represent the top left three letters \n\
      with (0,0) being the letter at the top left corner, (0,1) being the \n\
-     letter directly right to it etc.";
-
-  (* Read user input and parse it into a list of tuples *)
-  let input = read_line () in
-  let locations =
-    input |> String.split_on_char ';'
-    |> List.map (fun s -> Scanf.sscanf s "(%d,%d)" (fun x y -> (x, y)))
-  in
-
-  (* Print the list of tuples *)
-  List.iter (fun (x, y) -> Printf.printf "(%d,%d)\n" x y) locations;
-
-  (* Check if the word is valid and print the result *)
-  if BuildBoard.is_valid_word2 locations board then
-    print_endline "The word is valid!"
-  else print_endline "The word is not valid."
+     letter directly right to it etc. Requires that the format of the input is\n\
+     is exactly like the example. That is, there can't be any spaces between or\n\
+     within the tuples and no non-tuple strings. So (0, 0) and 'hello' would\n\
+     be invalid inputs.";
+  repl eval_input
